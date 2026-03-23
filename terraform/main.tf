e2bc0d324a10bc7ca1547c6761f9c884d0a028f2
Olivier Cervello <ocervello@freelabz.com>
2026-03-23 16:06:41 +0100

    feat: migrate Terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,14 @@
 terraform {
-  required_version = ">= 1.7"
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
+  }
+
+  backend "gcs" {
+    bucket = "valued-listener-491107-s3-tf-state"
+    prefix = "helloworld/state"
   }
 }
 
